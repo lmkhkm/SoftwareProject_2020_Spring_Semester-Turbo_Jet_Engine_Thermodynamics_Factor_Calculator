9180f969a77cb43b3e3f8e7950f5f920bbde8ee6
HeoJunKu <lmkhkm@naver.com>
2020-04-15 13:29:55 +0900

    이미지 파일 추가 및자바 GUI 기본 UI 배치

--- JavaProjectFiles/Jet_Engine_ThermoDynamics_Factor_Calculator/src/JavaGUI.java
+++ JavaProjectFiles/Jet_Engine_ThermoDynamics_Factor_Calculator/src/JavaGUI.java
@@ -1,12 +1,35 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class JavaGUI extends JFrame
 {
 	public JavaGUI() 
 	{
 		setTitle("고도 - 압력");
-		setSize(600,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentpane = new Container();
+		contentpane = getContentPane();
+		contentpane.setLayout(new BorderLayout());
+		
+		JPanel leftPanel = new JPanel();
+		JPanel rightPanel = new JPanel();
+		JPanel downPanel = new JPanel();
+		
+		JButton calculateButton = new JButton("Calculate");
+		
+		contentpane.add(leftPanel, BorderLayout.WEST);
+		contentpane.add(rightPanel, BorderLayout.EAST);
+		contentpane.add(downPanel, BorderLayout.SOUTH);
+		
+		leftPanel.add(new JLabel("고도 입력"));
+		leftPanel.add(new JTextField(10));
+		rightPanel.add(calculateButton);
+		downPanel.add(new JLabel("압력은 : "));
+		
+		setSize(350,150);
 		setVisible(true);
+		
 	}
 	
 }
